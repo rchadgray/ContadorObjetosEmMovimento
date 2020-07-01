c85746446388c194dfd334173b1e2c8269ea9402
Chad Gray <rchadgray@gmail.com>
2020-07-01 15:10:02 -0400

    translated more to english

--- ContadorObjetosEmMovimento.py
+++ ContadorObjetosEmMovimento.py
@@ -4,29 +4,29 @@ import cv2
 import numpy as np
 import imutils
 
-#variaveis globais
+#global variables
 width = 0
 height = 0
-ContadorEntradas = 0
-ContadorSaidas = 0
-AreaContornoLimiteMin = 2000  #this value is empirical. Adjust it according to your need. 
-ThresholdBinarizacao = 70  #this value is empirical. Adjust it according to your need.
-OffsetLinhasRef = 150  #this value is empirical. Adjust it according to your need. reference line offset
+CountEntries = 0
+CountExits = 0
+ContourAreaMin = 2000  #this value is empirical. Adjust it according to your need. 
+ThresholdBinary = 70  #this value is empirical. Adjust it according to your need.
+OffestReferenceLine = 150  #this value is empirical. Adjust it according to your need. reference line offset
 
 #Check if the detected body is entering the monitored area
-def TestaInterseccaoEntrada(y, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-    DiferencaAbsoluta = abs(y - CoordenadaYLinhaEntrada)
+def TestEntryIntersection(y, YLineCoordinateEntry, YLineCoordinateExit):
+    ABSDifference = abs(y - YLineCoordinateEntry)
 
-    if ((DiferencaAbsoluta <= 2) and (y < CoordenadaYLinhaSaida)):
+    if ((ABSDifference <= 2) and (y < YLineCoordinateExit)):
         return 1
     else:
         return 0
 
 #Check if the detected body is leaving the monitored area
-def TestaInterseccaoSaida(y, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-    DiferencaAbsoluta = abs(y - CoordenadaYLinhaSaida)    
+def TestExitIntersection(y, YLineCoordinateEntry, YLineCoordinateExit):
+    ABSDifference = abs(y - YLineCoordinateExit)    
 
-    if ((DiferencaAbsoluta <= 2) and (y > CoordenadaYLinhaEntrada)):
+    if ((ABSDifference <= 2) and (y > YLineCoordinateEntry)):
         return 1
     else:
         return 0
@@ -37,7 +37,7 @@ camera = cv2.VideoCapture(0)
 camera.set(3,640)
 camera.set(4,480)
 
-PrimeiroFrame = None
+FirstFrame = None
 
 # do some frame readings before considering the analysis
 # motive: some cameras may take longer to "get used to the brightness" 
@@ -64,14 +64,14 @@ while True:
 
     # as the comparison is made between two subsequent images, 
     # if the first frame is null (that is, the first "passed" in the loop), this is initialized
-    if PrimeiroFrame is None:
-        PrimeiroFrame = FrameGray
+    if FirstFrame is None:
+        FirstFrame = FrameGray
         continue
 
     #every absolute difference between starting frame and current frame (background subtraction)
     #also, make the frame binary with subtracted background 
-    FrameDelta = cv2.absdiff(PrimeiroFrame, FrameGray)
-    FrameThresh = cv2.threshold(FrameDelta, ThresholdBinarizacao, 255, cv2.THRESH_BINARY)[1]
+    FrameDelta = cv2.absdiff(FirstFrame, FrameGray)
+    FrameThresh = cv2.threshold(FrameDelta, ThresholdBinary, 255, cv2.THRESH_BINARY)[1]
     
     # makes the binarized frame expand, in order to eliminate "holes" / white areas within detected outlines. 
     # In this way, detected objects will be considered a "mass" of black color 
@@ -81,28 +81,28 @@ while True:
     cnts = imutils.grab_contours(cnts)
     # _, cnts, _ = cv2.findContours(FrameThresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    QtdeContornos = 0
+    ContourCount = 0
 
     #draw reference lines
-    CoordenadaYLinhaEntrada = (height / 2) - OffsetLinhasRef
-    CoordenadaYLinhaSaida = (height / 2) + OffsetLinhasRef
-    #print(CoordenadaYLinhaEntrada) #90
-    #print(CoordenadaYLinhaSaida) #390
+    YLineCoordinateEntry = (height / 2) - OffestReferenceLine
+    YLineCoordinateExit = (height / 2) + OffestReferenceLine
+    #print(YLineCoordinateEntry) #90
+    #print(YLineCoordinateExit) #390
     #print(width) #640
     #cv2.line(Frame, (0, 90), (640, 90), (255, 0, 0), 2)
-    cv2.line(Frame, (0, int(CoordenadaYLinhaEntrada)), (int(width), int(CoordenadaYLinhaEntrada)), (255, 0, 0), 2)
+    cv2.line(Frame, (0, int(YLineCoordinateEntry)), (int(width), int(YLineCoordinateEntry)), (255, 0, 0), 2)
     #cv2.line(Frame, (0, 390), (640, 390), (0, 0, 255), 2)
-    cv2.line(Frame, (0, int(CoordenadaYLinhaSaida)), (int(width), int(CoordenadaYLinhaSaida)), (0, 0, 255), 2)
+    cv2.line(Frame, (0, int(YLineCoordinateExit)), (int(width), int(YLineCoordinateExit)), (0, 0, 255), 2)
 
 
     #Sweep all found contours
     for c in cnts:
         #contours of very small area are ignored.
-        if cv2.contourArea(c) < AreaContornoLimiteMin:
+        if cv2.contourArea(c) < ContourAreaMin:
             continue
 
         #For debugging purposes, account for number of contours found
-        QtdeContornos = QtdeContornos+1    
+        ContourCount = ContourCount+1    
 
         # get contour coordinates (actually, a rectangle that can cover the entire contour) and
         #emphasizes the outline with a rectangle.
@@ -112,19 +112,19 @@ while True:
         cv2.rectangle(Frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         #determines the center point of the contour and draws a circle to indicate
-        CoordenadaXCentroContorno = (x+x+w)/2
-        CoordenadaYCentroContorno = (y+y+h)/2
-        PontoCentralContorno = (int(CoordenadaXCentroContorno),int(CoordenadaYCentroContorno))
-        print(PontoCentralContorno)
-        cv2.circle(Frame, PontoCentralContorno, 1, (0, 0, 0), 5)
+        ContourCenterXCoordinate = (x+x+w)/2
+        ContourCenterYCoordinate = (y+y+h)/2
+        ContourCenter = (int(ContourCenterXCoordinate),int(ContourCenterYCoordinate))
+        print(ContourCenter)
+        cv2.circle(Frame, ContourCenter, 1, (0, 0, 0), 5)
 
         #testa intersection of the centers of the contours with the reference lines
         #dthis way, it counts which contours crossed which lines (in a certain direction)
-        if (TestaInterseccaoEntrada(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
-            ContadorEntradas += 1
+        if (TestEntryIntersection(ContourCenterYCoordinate,YLineCoordinateEntry,YLineCoordinateExit)):
+            CountEntries += 1
 
-        if (TestaInterseccaoSaida(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
-            ContadorSaidas += 1
+        if (TestExitIntersection(ContourCenterYCoordinate,YLineCoordinateEntry,YLineCoordinateExit)):
+            CountExits += 1
 
         #If necessary, uncomment the lines below to show the frames used in image processing
         #cv2.imshow("Frame binarizado", FrameThresh)
@@ -133,13 +133,13 @@ while True:
         #cv2.waitKey(1);
 
 
-    foo = 'Contours Found: ' + str(QtdeContornos)
+    foo = 'Contours Found: ' + str(ContourCount)
     print(foo)
 
     #Write in the image the number of people who entered or left the guarded area
-    cv2.putText(Frame, "Entry: {}".format(str(ContadorEntradas)), (10, 50),
+    cv2.putText(Frame, "Entry: {}".format(str(CountEntries)), (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
-    cv2.putText(Frame, "Exit: {}".format(str(ContadorSaidas)), (10, 70),
+    cv2.putText(Frame, "Exit: {}".format(str(CountExits)), (10, 70),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.imshow("Original", Frame)
     cv2.waitKey(1);
